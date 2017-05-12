152358eeb3219d8e69039536300d773e8c954ce2
Rusty Bower <rusty@rustybower.com>
2017-05-12 20:21:26 -0300

    Reorging

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-import os
-
-from flask import Flask
-
-app = Flask(__name__)
-# app.config.from_pyfile('settings.cfg')
-app.secret_key = os.urandom(128)
-
-from site.web.views import index
-
-app.register_blueprint(index.mod)
+from site.web import app
 
 app.run(debug=True)
---
+++ site/web/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+# app.config.from_pyfile('settings.cfg')
+app.secret_key = os.urandom(128)
+
+from site.web.views import index
+
+app.register_blueprint(index.mod)
