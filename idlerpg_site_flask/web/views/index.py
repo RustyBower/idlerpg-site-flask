df9f95385d7f496798e2ed8b50274710bd55cf1c
Rusty Bower <rusty@rustybower.com>
2017-05-12 20:31:28 -0300

    Fixing typos

--- idlerpg_site_flask/web/views/index.py
+++ idlerpg_site_flask/web/views/index.py
@@ -1,11 +1,9 @@
 from flask import Blueprint, render_template
-from scoring_engine.engine.config import config
 
 mod = Blueprint('welcome', __name__)
 
 
 @mod.route('/')
 @mod.route("/index")
-def home():
-    sponsorship_images = config.sponsorship_images
-    return render_template('welcome.html', sponsorship_images=sponsorship_images)
+def index():
+    return render_template('index.html')
